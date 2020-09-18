fe217ab5d86892c2531ee2e5ca0fbc49785e6133
pawel.lelental <pawel.lelental@ihsmarkit.com>
2020-09-18 14:06:56 +0200

    first commit

---
+++ Controllers/CoffeeShopController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeShopController : ControllerBase
+    {
+        private readonly ILogger<CoffeeShopController> _logger;
+
+        public CoffeeShopController(ILogger<CoffeeShopController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Coffee> Get()
+        {
+            return new[]
+            {
+                new Coffee
+                {
+                    Name = "Palus Putredinis",
+                    Type = "Espresso - Blend",
+                    Manufacturer = "NieCzapla"
+                },
+                new Coffee
+                {
+                    Name = "Marley One",
+                    Type = "Arabica - Beans",
+                },
+                new Coffee
+                {
+                    Name = "Lucafe",
+                    Type = "Arabica - Beans",
+                },
+            };
+        }
+    }
+}
